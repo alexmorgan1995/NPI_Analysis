cb5cb5ec1836df66a4d8459ca18e43b6904db756
alexmorgan1995 <a.morgan@outlook.com>
2020-09-04 13:32:39 +0100

    Update SUPPLEMENTARY_5_Scenario_conreduc_SEIR.R
    Adding annotations to the script

--- SUPPLEMENTARY_5_Scenario_conreduc_SEIR.R
+++ SUPPLEMENTARY_5_Scenario_conreduc_SEIR.R
@@ -4,16 +4,15 @@ rm(list=ls())
 setwd("C:/Users/amorg/Documents/PhD/nCoV Work/Figures/WriteUpAnalysis/supplementary")
 start_time <- Sys.time()
 
-
-#Common FUnction
+# Common Functions --------------------------------------------------------
 #Generation Time Function
 GenTime <- function(T2, R0) {
   G = T2 * ((R0-1)/log(2))
   return(G)
 }
 
-# SEIR MODEL --------------------------------------------------------------
-#Function to Obtain Betas for the Model 
+# SEIR MODEL - FUNCTIONS --------------------------------------------------------------
+#Function defining beta(t) for the SEIR model 
 combbeta <- function(scen, time, tstart, t_dur, cmin) {
   gamma <- 1/GenTime(3, 2.8)
   betascale <- (2.8*gamma)*0.7
@@ -66,8 +65,9 @@ SEIR <- function(time, state, parameters) {
   })
 } 
 
-# Baseline Model Analysis - 5 Scenarios -----------------------------------
+# SEIR MODEL - Trajectory plot with baseline parameters  -----------------------------------
 
+#Initial conditions and parameters
 init <- c(S = 0.99999, E = 0, I = 0.00001, R = 0, C = 0)
 times <- seq(0,400,by = 1)
 parms = c(gamma = 1/GenTime(3, 2.8),
@@ -77,23 +77,23 @@ parms = c(gamma = 1/GenTime(3, 2.8),
           t_dur = 12*7,
           cmin = 0.4)
 
+#Initiate an empty dataframe
 datalist <- list()
 
-for(j in 1:length(seq(1,5))) {
+for(j in 1:length(seq(1,5))) { #For each of the 5 scenarios conduct a model run
   datalist[[j]] <- local({
     j=j
-    data <- data.frame(matrix(nrow = 9, ncol = 0))
-    explor_scen <- c(0, seq(1,5)[j])
-    
+    data <- data.frame(matrix(nrow = 9, ncol = 0)) #Create empty dataframe 
+    explor_scen <- c(0, seq(1,5)[j]) #Define the scenario based on the current model iteration
     
     for(i in 1:2) {
       
       parms["scen"] <- explor_scen[i]
-      
-      if(parms["scen"] != 0 && parms["scen"] != 1) {
+      if(parms["scen"] != 0 && parms["scen"] != 1) { #if the scenario is not 1 or 0 then double dt
         parms["t_dur"] = 24*7
       }
       
+      #Run the model 
       out <- cbind(data.frame(ode(y = init, func = SEIR, times = times, parms = parms)), 
                    "group" =  c("baseline", "scenario")[i], 
                    "beta" = combbeta(explor_scen[i], times, parms[["tstart"]], parms[["t_dur"]], parms[["cmin"]]))
@@ -101,15 +101,17 @@ for(j in 1:length(seq(1,5))) {
       data <- rbind(data, out)
     }
     
+    #Manipulate the data into a suitable format for plotting 
     plotdata <- melt(data, id.vars = c("time", "group"), measure.vars = ("I"))
     plotbeta <- melt(data, id.vars = c("time", "group"), measure.vars = ("beta"))
     plotre <- melt(data, id.vars = c("time", "group"), measure.vars = ("re"))
+    #Identify the peak prevalence and the attack rate for each of the scenarios + text for annotation on the plot
     peak <- round(max(out$I), 3)
     cum <- round(max(out$C), 3)
-    
     datatext <- data.frame(x = c(200, 200), y = c(0.15, 0.135), label = c( paste0("italic(I)[italic(max)]", " ==", peak), 
                                                                            paste0("italic(I)[italic(c)](italic(t)[italic(max)])", " ==", cum)))
     
+    #Generic plotting (common across scenarios) for the peak prevalence and the attack rate
     p1 <- ggplot(data = plotdata, aes(x = time, y = value, color = group , alpha= group)) + theme_bw() +
       scale_y_continuous(limits = c(0 , 0.16),expand = c(0,0)) + 
       scale_x_continuous( expand = c(0, 0)) + scale_alpha_manual(values = c(0.35, 1)) + scale_color_manual(values = c("darkred", "darkred"))
@@ -122,8 +124,10 @@ for(j in 1:length(seq(1,5))) {
       geom_hline(yintercept = 1, size = 1.1, lty = 2, col = "black") + geom_line(size = 1.02, stat = "identity") + 
       scale_alpha_manual(values = c(0.35,1)) + scale_color_manual(values = c("darkblue", "darkblue"))
     
+    #Identifying the region on the plot where the intervention is occuring
     shade <- data.frame(xmin =  parms[["tstart"]], xmax = parms[["tstart"]]+(parms[["t_dur"]]), ymin = 0, ymax = Inf)
     
+    #Scenario specific plotting (removing the axis labels and titles for certain panels)
     if(parms[["scen"]]  == 1) {
       p1 <- p1 + geom_rect(data = shade, inherit.aes = F, aes(ymin = ymin, ymax = ymax, xmin = xmin, xmax = xmax), alpha = 0.2,
                            fill = "darkblue") + geom_line(size = 1.1, stat = "identity") + labs(x ="Time (Days)", y = "Prevalence", col = "", title = "Scenario 1") +
@@ -217,30 +221,33 @@ for(j in 1:length(seq(1,5))) {
         scale_y_continuous(limits = c(0 , 2), expand = c(0,0)) 
     }
     
+    #Combine the trajectory plot, beta(t) and re(t) plots together
     combplot <- ggarrange(p1,p2,p3, nrow = 3, ncol = 1, common.legend = TRUE, legend = "none", align = "v",heights = c(1, 0.4, 0.5))
-    dump <- list(combplot, plotdata, max(out$C))
+    dump <- list(combplot, plotdata, max(out$C)) #Retaining the plots and dataframes needed outside of the local() for-loop
     return(dump)
   })
 }
 
+#Combine the plots together for each scenario
 combplot <- ggarrange(NULL,datalist[[1]][[1]], datalist[[2]][[1]], datalist[[3]][[1]], datalist[[4]][[1]], datalist[[5]][[1]], NULL,nrow = 1, ncol = 7, 
                       legend = "none", align = "h", widths = c(0.1,1,0.9,0.9,0.9,0.9,0.1))
 
 
+#Identify the peak prevalence and the attack rate for each scenario. 
 for(i in 1:5) {
   print(datalist[[i]][[2]][datalist[[i]][[2]]$group == "scenario",]
         [which.max(datalist[[i]][[2]]$value[datalist[[i]][[2]]$group == "scenario"]),])
   print(datalist[[i]][[3]])
 }
 
+#Save the final trajectory plot int he working directory
 ggsave(combplot, filename = "5_scenarios_SEIR.png", dpi = 300, type = "cairo", width = 14, height = 10, units = "in")
 
+# MODELLING SUSTAINABLE REDUCTIONS TO TRANSMISSION AFTER NPIS MODEL - FUNCTIONS ----------------------------------------------------------
 
-# CONSTANT REDUCTION MODEL ------------------------------------------------
+#Function describing beta(t) for an initial optimisable NPI followed by a constant, indefinite reduction to transmission
+#Note that the chracteristics of the constant reduction to transmission are defined by a start date and a magnitude - contstart and cmin2
 
-# Model Functions ----------------------------------------------------------
-
-#Multiple Interventions
 combbetamult <- function(scen, time, tstart1, t_dur1, contstart, cmin1, cmin2) {
   gamma <- 1/GenTime(3, 2.8)
   betascale <- (2.8*gamma)*0.7
@@ -290,9 +297,10 @@ combbetamult <- function(scen, time, tstart1, t_dur1, contstart, cmin1, cmin2) {
   return(output)
 }
 
+#Example plotting of beta(t)
 plot(seq(0,200),combbetamult(3, seq(0,200), 20, 2*7, 100, 0.1, 0.5))
 
-#SEIR set of ODEs - Repeated
+#SIR set of ODEs
 SIRmulti <- function(time, state, parameters) {
   with(as.list(c(state, parameters)), {
     beta <- combbetamult(scen, time, tstart1, t_dur1, contstart, cmin1, cmin2)
@@ -306,16 +314,16 @@ SIRmulti <- function(time, state, parameters) {
   })
 } 
 
+# MODELLING SUSTAINABLE REDUCTIONS TO TRANSMISSION AFTER NPIS MODEL - Trajectory plots and sensitivity analyses --------------------------------------------------
 
-# Model the Intervention --------------------------------------------------
-
+#Range of values explored in the sensitivity analysis
 sens <- list("tstart1" = seq(0,100, by = 1),
              "cmin1" = seq(0,1, by = 0.01),
              "t_dur1" = seq(1,100, by = 1))
 
+#Initial conditions and parameter values
 init <- c(S = 0.99999, I = 0.00001, R = 0, C = 0)
 times <- seq(0,300,by = 1)
-
 parms = c(gamma = 1/GenTime(3, 2.8),
           scen = 1,
           tstart1 = 52,
@@ -324,31 +332,40 @@ parms = c(gamma = 1/GenTime(3, 2.8),
           cmin1 = 0.4,
           cmin2 = 0.5)
 
+#Empty list for trajectory plot and sensitivity analysis data storage
 outcomelistcmin <- list()
 
-for(z in 1:5) {
+#This analysis loop both conducts a basic trasejectory plot for I(t) and beta(t) and
+#Also explores the 
+
+for(z in 1:5) { #Run for each of the 5 scenarios and store each trajectory plot separately
   outcomelistcmin[[z]] <- local({
     
-    parms[["scen"]] <- z
+    parms[["scen"]] <- z #Define the scenario model parameter for each analysis loop 
     
+    ##TRAJECTORY PLOT ANALYSIS
+    #Run the model and obtain I(t) and beta(t)
     outtraj <- cbind(data.frame(ode(y = init, func = SIRmulti, times = times, parms = parms)),
                  "beta" =  combbetamult(parms[["scen"]], times, parms[["tstart1"]], parms[["t_dur1"]], parms[["contstart"]], parms[["cmin1"]], parms[["cmin2"]]))
+    
+    #Identify the peak prevalence and attack rate and define a text box for each scenario with these two outcome measures
     peak <- round(max(outtraj$I), 3)
     cum <- round(max(outtraj$C), 3)
-    
-    datatext <- data.frame(x = c(150, 150), y = c(0.07, 0.06), label = c( paste0("italic(I)[italic(max)]", " ==", peak), 
+    datatext <- data.frame(x = c(150, 150), y = c(0.07, 0.06), label = c(paste0("italic(I)[italic(max)]", " ==", peak), 
                                                                             paste0("italic(I)[italic(c)](italic(t)[italic(max)])", " ==", cum)))
     
+    ##SENSITIVITY ANALYSIS
+    #Create an empty dataframe to store sensitivity analysis results 
     datasens <- data.frame(matrix(nrow = 0, ncol = 5))
     
-    for(i in 1:3) {
-      sensitivity <- sens[[i]]
+    for(i in 1:3) { #Run for each parameter explored 
+      sensitivity <- sens #Define what parameter you are exploring 
       data <- data.frame(matrix(nrow = 0, ncol = 5))
       
-      for(j in 1:length(sensitivity)) {
-        parms1 <- parms
-        parms1[names(sens)[i]] <- sensitivity[j]
-        out <- data.frame(ode(y = init, func = SIRmulti, times = times, parms = parms1))
+      for(j in 1:length(sensitivity)) { #For the identified parameter - run the model for the explored range
+        parms1 <- parms #create a dummy vector for the model parameters 
+        parms1[names(sens)[i]] <- sensitivity[j] #For the explored parameter and value - assign it to the actual parameter vector
+        out <- data.frame(ode(y = init, func = SIRmulti, times = times, parms = parms1)) #Run the model
         data[j,1] <- max(out$I)
         data[j,2] <- max(out$C)
         data[j,3] <- sensitivity[j]
@@ -360,11 +377,13 @@ for(z in 1:5) {
     
     colnames(datasens) <- c("peak", "cum", "sensval", "sens", "scen")
     
-    print(outtraj)
+    print(outtraj) # Progress bar
     
+    #Define the period of the intervention for both the NPI (blue) and the constant reduction to transmission (red)
     shade <- data.frame(xmin =  c(parms[["tstart1"]], parms[["contstart"]]), 
                         xmax = c(parms[["tstart1"]]+parms[["t_dur1"]], Inf), ymin = c(0,0), ymax = c(Inf,Inf))
     
+    #Generic Plotting for the trajectory plot and the beta(t) plot
     p1 <- ggplot(data = outtraj, aes(x = time, y = I)) + theme_bw() + scale_y_continuous(limits = c(0,0.075), expand = c(0,0)) + 
       scale_x_continuous(expand = c(0, 0)) + geom_rect(data = shade, inherit.aes = F, aes(ymin = ymin, ymax = ymax, xmin = xmin, xmax = xmax), alpha = 0.2,
                                                        fill = c("darkblue", "darkred")) + geom_line(size = 1.1, stat = "identity") 
@@ -374,6 +393,8 @@ for(z in 1:5) {
               fill = c("darkblue", "darkred")) + geom_line(size = 1.1, stat = "identity") + 
       geom_line(size = 1.1, stat = "identity", col = "darkblue") + scale_x_continuous(expand = c(0, 0))
     
+    
+    #Scenario specific plotting for the peak prevalence and attack rate plots (removing axis labels for certain panels)
     if(parms[["scen"]] == 1) { 
            p1 <- p1 + theme(axis.text.x = element_blank(), axis.text.y = element_text(size=13), axis.title.y= element_text(size=15), 
                             axis.title.x = element_blank(), plot.margin=unit(c(0.2,0.4,0.2,0.4),"cm"),  
@@ -402,16 +423,19 @@ for(z in 1:5) {
   })
 }
 
+#Combining the scenarios for the trajectory + beta(t) plots together
 traj <- ggarrange(NULL,outcomelistcmin[[1]][[1]], outcomelistcmin[[2]][[1]], outcomelistcmin[[3]][[1]], outcomelistcmin[[4]][[1]], outcomelistcmin[[5]][[1]],NULL,
                   nrow = 1, ncol =7, widths = c(0.1,1,0.75,0.75,0.75,0.75,0.1))
 
+#Formatting the sensitivity analysis for plotting
 sensitivityanal <- data.frame(matrix(nrow = 0, ncol = 5))
 for(i in 1:5) {
   sensitivityanal <- rbind(sensitivityanal, outcomelistcmin[[i]][[2]])
 }
 
+#Need to plot the sensitivity analysis for the explord parameter for the peak prevalence (p1) and the attack rate (p2) for each scenario
 
-#
+#MAGNITUDE (c_min)
 cminpeak <- ggplot(sensitivityanal[sensitivityanal$sens == "cmin1",], aes(x = sensval, y = peak, col = scen)) + geom_line(size = 1.02) + theme_bw()  + 
   theme(legend.position = "bottom", legend.title = element_text(size=15), legend.text=element_text(size=18), legend.spacing.x = unit(0.3, 'cm'),
         axis.text.x=element_blank(), axis.text.y=element_text(size=13), axis.title.y=element_text(size=15), axis.title.x = element_blank(), 
@@ -426,8 +450,8 @@ cmincum <- ggplot(sensitivityanal[sensitivityanal$sens == "cmin1",], aes(x = sen
         plot.margin=unit(c(0.2,0.4,0.2,0.1),"cm")) + 
   labs(x = expression("Magnitude "~"("~italic("c"["min"]~")")), y = expression(italic("I"["c"]~"("~tmax~")")), col = "Scenario")+
   scale_x_continuous(expand = c(0, 0)) + scale_y_continuous(limits = c(0,0.75),expand = c(0, 0))
-#
 
+#TSTART (t_p)
 tstartpeak <- ggplot(sensitivityanal[sensitivityanal$sens == "tstart1",], aes(x = sensval, y = peak, col = scen)) + geom_line(size = 1.02) + theme_bw()  + 
   theme(legend.position = "bottom", legend.title = element_text(size=15), legend.text=element_text(size=18), legend.spacing.x = unit(0.3, 'cm'),
         axis.text.x=element_blank(),axis.text.y=element_blank(), axis.title.y=element_blank(),axis.title.x = element_blank(), 
@@ -442,7 +466,7 @@ tstartcum <- ggplot(sensitivityanal[sensitivityanal$sens == "tstart1",], aes(x =
   labs(x = expression("Trigger "~"("~italic("t"["p"]~")")), y = expression(italic("I"["c"]~"("~infinity~")")), col = "Scenario")+
   scale_x_continuous(expand = c(0, 0)) + scale_y_continuous(limits = c(0,0.75),expand = c(0, 0))
 
-#
+#DURATION (d_t)
 durpeak <- ggplot(sensitivityanal[sensitivityanal$sens == "t_dur1",], aes(x = sensval, y = peak, col = scen)) + geom_line(size = 1.02) + theme_bw()  + 
   theme(legend.position = "bottom", legend.title = element_text(size=15), legend.text=element_text(size=18), legend.spacing.x = unit(0.3, 'cm'),
         axis.text.x=element_blank(),axis.text.y=element_blank(), axis.title.y=element_blank(),axis.title.x = element_blank(), 
@@ -457,15 +481,16 @@ durcum <- ggplot(sensitivityanal[sensitivityanal$sens == "t_dur1",], aes(x = sen
   labs(x = expression("Duration "~"("~italic("d"["t"]~")")), y = expression(italic("I"["c"]~"("~infinity~")")), col = "Scenario")+
   scale_x_continuous(limits = c(0,100), expand = c(0, 0)) + scale_y_continuous(limits = c(0,0.75), expand = c(0, 0))
 
-#Plots 
-
+#Creating a unifying plot for the sensitivity analysis
 peak <- ggarrange(NULL,cminpeak, tstartpeak, durpeak,NULL,
                   NULL,cmincum, tstartcum, durcum,NULL,
                   nrow = 2, ncol =5, common.legend = TRUE, legend = "bottom", align = "v", widths = c(0.1,1,1,1,0.1), heights = c(0.8,1))
 
+#Combining the trajectory/beta(t) + sensitivity analysis plots together 
 comb <- ggarrange(NULL, traj, NULL,peak, nrow = 4, ncol = 1, heights = c(0.08,1,0.1,0.8),
                   labels = c("","A","", "B"),font.label = c(size = 25), vjust = -0.3, hjust = -0.4)
 
+#Save the plot to the working directory
 ggsave(comb, filename = "5_scenarios_multiadapt_const.png", dpi = 300, type = "cairo", width = 11, height = 10, units = "in")
 
 
